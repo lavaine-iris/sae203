6e3c2f0883ed104066090c2fcdc6e0e4176c5ffd
lavaine-iris <iris.lavaine@edu.univ-fcomte.fr>
2022-05-30 18:29:41 +0200

    Page Mentions Légales

--- src/views/MentionsView.vue
+++ src/views/MentionsView.vue
@@ -7,5 +7,48 @@
       </div>
       <div class="h-1 w-1/3 bg-yellow-500"></div>
     </div>
+    <div class="m-4">
+      <p class="text-sm font-normal">
+        Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la confiance dans l’économie
+        numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l""Utilisateur", du site snowpop.fr
+        , ci-après le "Site", les présentes mentions légales. La connexion et la navigation sur le Site par l’Utilisateur implique
+        acceptation intégrale et sans réserve des présentes mentions légales. Ces dernières sont accessibles sur le Site à la rubrique «
+        Mentions légales ».
+      </p>
+      <div class="my-8">
+        <h4 class="text-base">ARTICLE 1 - L'EDITEUR</h4>
+        <p class="text-sm font-normal">
+          L’édition et la direction de la publication du Site est assurée par LAVAINE IRIS, domiciliée à Besançon, dont le numéro de
+          téléphone est 06.26.93.87.89, et l'adresse e-mail lavaineiris@gmail.com.
+        </p>
+      </div>
+      <div class="my-8">
+        <h4>ARTICLE 2 - L'HEBERGEUR</h4>
+        <p class="text-sm font-normal">
+          L'hébergeur du Site est la société Infomaniak, dont le siège social est situé à Genève , avec le numéro de téléphone : +41 22 820
+          35 44.
+        </p>
+      </div>
+      <div class="my-8">
+        <h4>ARTICLE 3 - ACCES AU SITE</h4>
+        <p class="text-sm font-normal">
+          Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant
+          découlant d’une nécessité de maintenance. En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être
+          tenu responsable.
+        </p>
+      </div>
+      <div class="my-8">
+        <h4>ARTICLE 4 - COLLECTE DES DONNEES</h4>
+        <p class="text-sm font-normal">
+          Le Site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée
+          conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. En vertu de la loi
+          Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et
+          d'opposition de ses données personnelles. L'Utilisateur exerce ce droit : · par mail à l'adresse email lavaineiris@gmail.com Toute
+          utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site, sans autorisation de l’Editeur
+          est prohibée et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété
+          intellectuelle et le Code civil.
+        </p>
+      </div>
+    </div>
   </main>
 </template>
